83e3e8af503d2c8be5801c40369ef0cc4e3ba3b5
[asdfwefsf] <[bethel5308@naver.com]>
2024-03-22 15:02:24 +0900

    Initial commit

---
+++ app/src/main/java/com/company/velogcomposenavigationsharedata/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.company.velogcomposenavigationsharedata
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.company.velogcomposenavigationsharedata.fiveWays.NavigationArgsSample
+import com.company.velogcomposenavigationsharedata.ui.theme.VelogComposeNavigationShareDataTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VelogComposeNavigationShareDataTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavigationArgsSample()
+                }
+            }
+        }
+    }
+}
+
